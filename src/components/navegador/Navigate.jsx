00e3fc2950dc7ecee09aff8657aa00ccd068b72b
ErickSalazar44 <erickearl22@gmail.com>
2023-07-06 20:38:42 -0500

    'modifique el tamaño de letra'

--- src/components/navegador/Navigate.jsx
+++ src/components/navegador/Navigate.jsx
@@ -9,9 +9,6 @@ const Navigate = () => {
             <a href='https://www.linkedin.com/in/erick-josep-salazar-8b8b6024b/' target="_blank" className="navigate__anclas linke">
                 <i className='bx bxl-linkedin'></i>
             </a>
-            <a href='https://www.instagram.com/erickcsalazar22/' target="_blank" className="navigate__anclas instagram">
-                <i className='bx bxl-instagram'></i>
-            </a>
             <a href="https://api.whatsapp.com/send?phone=51934219123" target="_blank" className="navigate__anclas wsp">
                 <i className='bx bxl-whatsapp'></i>
             </a>
