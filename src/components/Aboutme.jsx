0d6202ff3e1e2f6fcdc5f570b8fdb6f80de99e63
ErickSalazar44 <erickearl22@gmail.com>
2023-09-27 20:43:12 -0500

    Cambios en text Aboutme

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -44,7 +44,7 @@ const Aboutme = () => {
                             <div className='window__sidebar'>1</div>
                             <div className='window__text'>
                                 <p className='window__parrafo'>
-                                Desarrollador junior Fron-tend con experiencia en la creación de interfaces de usuario. Poseo habilidades en HTML, CSS, JavaScript y ReactJS, Me gusta utilizar mi conocimiento para construir páginas web de alta calidad. Cuando no estoy ampliando mis conocimientos en el desarrollo web, disfruto dibujar, videojuegos y hacer deporte al iré libre, Estoy entusiasmado por seguir creciendo profesionalmente en el ámbito del desarrollo Fron-tend y aplicar mis habilidades técnicas junto con mi creatividad en futuros proyectos desafiantes.
+                                Experiencia en la creación de interfaces de usuario. Poseo habilidades en JavaScript, ReactJS, CSS y Node. Me gusta utilizar mi conocimiento para construir páginas web de alta calidad. Cuando no estoy ampliando mis conocimientos en el desarrollo web, disfruto de la lectura, videojuegos y hacer deporte al aire libre. Estoy entusiasmado por seguir creciendo profesionalmente en el ámbito del desarrollo Front-end y aplicar mis habilidades técnicas junto con mi creatividad en futuros proyectos desafiantes.
                                 </p>
                                 <svg
                                     className='content-cursor'
